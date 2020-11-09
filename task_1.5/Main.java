85127918d5ecfef6250aaa885fb91e558ef3f179
spacejets <74203755+spacejets@users.noreply.github.com>
2020-11-09 21:23:34 +0300

    Create Main.java

---
+++ task_1.5/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new  Scanner (System.in);
+int a = (9/5);
+int c = scan.nextInt();
+int b = 32;
+        System.out.println(a*c+b);
+    }
+}
