292f3332094889ecc272f352825829fe46a52f2e
spacejets <74203755+spacejets@users.noreply.github.com>
2020-11-09 20:45:28 +0300

    Create Main.java

---
+++ task_1.1/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Biden ";
+        int age = 72;
+        String city = " Ufa";
+        System.out.println(name+age+city);
+
+
+    }
+}
