511b83428662601b2b6b170d9efabbac509d98ff
spacejets <74203755+spacejets@users.noreply.github.com>
2020-11-11 21:20:22 +0300

    Create Main.java

---
+++ task_2.9/Main.java
@@ -0,0 +1,15 @@
+/*
+Задание: Дан массив с числами. Проверьте, что в этом массиве есть число 5. Если есть - выведите 'да', а если нет - выведите 'нет'.
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        int[] et = {1, 2, 3, 4, 6};
+        for (int i = 0; i < et.length; i++)
+            if (et[i] == 5){
+                System.out.println("yes");
+                break;}
+    else {System.out.println("no");
+    break;}
+    }
+}
