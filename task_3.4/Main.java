4b92a682d145c75394abcc8f881aa25305118078
spacejets <74203755+spacejets@users.noreply.github.com>
2020-11-12 20:45:13 +0300

    Create Main.java

---
+++ task_3.4/Main.java
@@ -0,0 +1,17 @@
+/*
+Задание: Сделайте функцию, которая параметрами принимает 2 числа. Если их сумма больше 10 - пусть функция вернет true, а если нет - false.
+*/
+
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 1;
+        int sum = 10;
+        if (a+b > sum)
+            System.out.println("true");
+        else
+            System.out.println("false");
+
+    }
+}
