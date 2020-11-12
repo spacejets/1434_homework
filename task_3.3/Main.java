c6dce07d4f0a46047f01f3d0c1a10ba80c3e2d8f
spacejets <74203755+spacejets@users.noreply.github.com>
2020-11-12 20:40:58 +0300

    Create Main.java

---
+++ task_3.3/Main.java
@@ -0,0 +1,17 @@
+/*
+Задание: Сделайте функцию, которая параметрами принимает 2 числа. Если эти числа равны - пусть функция вернет true, а если не равны - false.
+*/
+
+
+public class Main {
+    public static void main(String[] args) {
+    int a = 125;
+    int b = 125;
+    if (a==b)
+
+        System.out.println("true");
+    else
+        System.out.println("false");
+
+    }
+}
