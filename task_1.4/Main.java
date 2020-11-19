dee9d1020d9eb9f1218ccd494126ffe001f2d8d7
spacejets <74203755+spacejets@users.noreply.github.com>
2020-11-19 18:03:56 +0300

    Update Main.java

--- task_1.4/Main.java
+++ task_1.4/Main.java
@@ -1,6 +1,10 @@
+/* Дана переменная number.
+Напиши программу, которая выводит на экран квадрат этой переменной (number * number). */
+
+
 public class Main {
     public static void main(String[] args) {
         int number = 25;
-        System.out.println(5*5);
+        System.out.println(number*number);
     }
 }
