c74ce049f868c049318a725e26e9e5971817f9eb
spacejets <74203755+spacejets@users.noreply.github.com>
2020-11-21 14:32:00 +0300

    Update Main.java

--- task_3.2/Main.java
+++ task_3.2/Main.java
@@ -15,32 +15,19 @@
 
 public class Main {
     public static int min(int a, int b, int c, int d) {
-        int m1;
-        int m2;
-        int m3;
-        if (a < b)
-            m1 = a;
-        else
-            m1 = b;
-        if (c < d)
-            m2 = c;
-        else
-            m2 = d;
-        if (m1 < m2)
-            m3 = m1;
-        else
-            m3 = m2;
-        return m3;
+        if (a < b && a<c && a<d)
+        return a;
+        if (b < a && b<c && b<d)
+        return b;
+        if ( c<a && c<b && c<d)
+        return c;
+        else return d;
     }
 
     public static int min(int a, int b) {
-        int m2;
         if (a < b)
-            m2 = a;
-        else
-            m2 = b;
-
-        return m2;
+        return a;
+        else return b;
 
     }
 
@@ -52,3 +39,5 @@ public class Main {
         System.out.println(min(-20, -10, -30, -40));
     }
 }
+
+
