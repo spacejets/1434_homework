e502c7a74cddad2b9227f9da7ea5d7883cc97403
spacejets <74203755+spacejets@users.noreply.github.com>
2020-11-25 21:50:18 +0300

    Update Main.java

--- task_1.6/Main.java
+++ task_1.6/Main.java
@@ -1,3 +1,6 @@
+/*
+Пользователь вводит число, необходимо увеличить это число на 15% и вывести результат на экран
+*/
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
