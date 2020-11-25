9366f8aa602cf9af2979fb97ae41f0db5410ab43
spacejets <74203755+spacejets@users.noreply.github.com>
2020-11-25 22:08:41 +0300

    Update Main.java

--- task_2.4/Main.java
+++ task_2.4/Main.java
@@ -1,6 +1,5 @@
 //Задание: Пользователь вводит сумму вклада и процент, который будет начисляться ежегодно. Отобразить размер вклада поочередно на ближайшие 5 лет.*/
 
-
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
@@ -9,20 +8,9 @@ public class Main {
         int a = scan.nextInt();
         System.out.println("Введите процентную ставку: ");
         int b = scan.nextInt();
-        int y1 = 0;
-        int y2 = 0;
-        int y3 = 0;
-        int y4 = 0;
-        int y5 = 0;
-            y1=a+a*b/100;
-            y2=y1+a*b/100;
-            y3=y2+a*b/100;
-            y4=y3+a*b/100;
-            y5=y4+a*b/100;
-
-            {System.out.println("размер вклада за первый год: " + y1 + "руб.");
-                System.out.println("размер вклада за второй год: " + y2 + "руб.");
-                System.out.println("размер вклада за третий год: " + y3 + "руб.");
-                System.out.println("размер вклада за четвертый год: " + y4 + "руб.");
-                System.out.println("размер вклада за пятый год: " + y5 + "руб.");}}
-            }
+        int p = a*b/100;
+        for (int i = 0; i < 6; i++) {
+            System.out.println("размер вклада: " + a + "руб.");
+            a+=p;
+            }}
+}
