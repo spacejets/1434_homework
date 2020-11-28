1a7c3be65885d085f2ad80575c0dd5fa02c65c69
spacejets <74203755+spacejets@users.noreply.github.com>
2020-11-28 12:55:45 +0300

    Create Main.java

---
+++ task_4.7/Main.java
@@ -0,0 +1,20 @@
+
+/*
++ Реализуй в классе Fox интерфейс Animal.
+Поменяй код так, чтобы в классе Fox был только один метод - getName.
+Учти, что создавать дополнительные классы и удалять методы нельзя!
+Требования:
+•	Интерфейс Animal должен быть реализован в классе Fox.
+•	В классе Fox должен быть реализован только один метод (getName).
+•	В интерфейсе Animal нужно объявить метод getColor.
+•	Дополнительные классы или интерфейсы создавать нельзя.
+*/
+public class Main {
+    public static void main(String[] args){}}
+interface Animal {
+    void getColor();}
+class Fox implements Animal {
+    public void getColor(){ }
+    public String getName() {
+        return "Fox";}
+}
